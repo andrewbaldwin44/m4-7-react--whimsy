83dcf5da734ee21a1baf68a14551ca585cfc7d9d
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-07-29 12:23:33 -0400

    Solution to exercise 5

--- src/components/LikeButton/index.js
+++ src/components/LikeButton/index.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import styled from "styled-components";
 
 import Heart from "./Heart";
+import PoppingCircle from "./PoppingCircle";
 import { TweetContext } from '../Tweet/TweetContext';
 
 const PARTICLE_COLORS = ["#e53935", "#1e88e5", "#43a047", "#fdd835", "#fb8c00"];
@@ -14,6 +15,7 @@ const LikeButton = ({ size = 40 }) => {
   return (
       <Wrapper style={{ width: size, height: size }}>
         <Heart width={heartSize} isToggled={isLikedByCurrentUser} />
+        {isLikedByCurrentUser && <PoppingCircle color="#E790F7" />}
       </Wrapper>
   );
 };
